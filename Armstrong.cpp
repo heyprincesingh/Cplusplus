f4d139a0b0ce34999e370bb77358fca1c40e4db0
Isra Wali <100406828+israwali@users.noreply.github.com>
2022-11-09 23:12:25 +0530

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-
 int main()
 {
-    int a,b = 0;
+    int a, b = 0;
     cin>>a;
     int copy = a;
-    while(a!=0)
+    while( a != 0 )
     {
-        b = b + pow(a%10,3);
-        a = a/10;
+        b = b + pow( a % 10 , 3 );
+        a = a / 10;
     }
     if(copy == b)
     {
